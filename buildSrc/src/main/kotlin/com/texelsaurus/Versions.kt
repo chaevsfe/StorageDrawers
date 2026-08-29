f25efd0cb82e6cde4ff141b549105e89b2e8b4e0
chaevsfe <pie@duck.com>
2026-08-29 01:48:41 -0400

    Bump the mod version to 19.1.8

--- buildSrc/src/main/kotlin/com/texelsaurus/Versions.kt
+++ buildSrc/src/main/kotlin/com/texelsaurus/Versions.kt
@@ -1,7 +1,7 @@
 package com.texelsaurus;
 
 object Versions {
-    const val mod = "19.1.7"
+    const val mod = "19.1.8"
     const val java = "25"
     const val minecraft = "26.2"
     const val minecraftRange = "[26.2,26.3)"
